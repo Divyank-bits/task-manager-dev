3bec72fd743be079dd710b44721459eb945733a6
Divyank Vyas <66863828+Divyank-bits@users.noreply.github.com>
2024-01-19 10:18:01 +0530

    move completed task to bottom

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = ({ tasks, onDelete, onUpdate }) => {
+  const completedTask = tasks.filter((task)=>task.completed);
+  const incompletedTask = tasks.filter((task)=>!task.completed);
+
+  const combineTask= [...incompletedTask, ...completedTask]
   return (
     <div className="container mt-4">
       <div className="card p-4 rounded shadow">
         <h3 className="card-title text-lg font-semibold mb-4">Tasks:</h3>
         <div className="row">
-          {tasks.map((task) => (
+          {combineTask.map((task) => (
             <div key={task._id} className="col-lg-4 mb-3">
               <Task task={task} onDelete={onDelete} onUpdate={onUpdate} />
             </div>
